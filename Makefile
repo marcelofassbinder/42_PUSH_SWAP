77a162c0cbe2f5bc3eca5d12e7d2305712ca1315
Marcelo Fassbinder Martins <mfassbin@c1r14s3.42porto.com>
2023-12-20 19:44:37 +0000

    push move added

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFILES = main.c check_input.c init_stack.c stack_utils.c swap.c rotate_rr.c rotate_r.c sort_three.c
+CFILES = main.c check_input.c init_stack.c stack_utils.c swap.c rotate_rr.c rotate_r.c push.c sort_three.c 
 OBJS = ${CFILES:.c=.o}
 RM = rm -rf
 CC = cc
@@ -13,14 +13,14 @@ $(NAME): $(OBJS) $(FTPRINTFA)
 	$(CC) $(CFLAGS) $(OBJS) $(FTPRINTFA) -o $(NAME)
 
 $(FTPRINTFA): $(FTPRINTFD)
-	make -C $(FTPRINTFD)
+	make -C $(FTPRINTFD) 
 
 clean:
 	$(RM) $(OBJS)
-	make clean -C $(FTPRINTFD)
+	make clean -C $(FTPRINTFD) 
 
 fclean: clean
 	$(RM) $(NAME)
-	make fclean -C $(FTPRINTFD)
+	make fclean -C $(FTPRINTFD) 
 
 re: fclean all
---
+++ push.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfassbin <mfassbin@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 18:11:54 by mfassbin          #+#    #+#             */
+/*   Updated: 2023/12/20 19:38:14 by mfassbin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	push_pb(t_stack_node **stack_a, t_stack_node **stack_b)
+{
+	t_stack_node	*old;
+
+	if (*stack_a == NULL)
+		return ;
+	old = *stack_a;
+	if (*stack_b == NULL)
+	{
+		append_node(stack_b, old->number, false);
+		*stack_a = (*stack_a)->next;
+		free(old);
+		return ;
+	}
+	append_node(stack_b, old->number, true);
+	*stack_a = (*stack_a)->next;
+	free(old);
+	ft_printf("pb\n");
+}
+void	push_pa(t_stack_node **stack_b, t_stack_node **stack_a)
+{
+	t_stack_node	*old;
+
+	if (*stack_b == NULL)
+		return ;
+	old = *stack_b;
+	if (*stack_a == NULL)
+	{
+		append_node(stack_a, old->number, false);
+		*stack_b = (*stack_b)->next;
+		free(old);
+		return ;
+	}
+	append_node(stack_a, old->number, true);
+	*stack_b = (*stack_b)->next;
+	free(old);
+	ft_printf("pa\n");
+}
+
